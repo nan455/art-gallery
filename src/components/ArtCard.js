f3d8bce21f94c5457ae52026e30f78c8be1dfd6e
nan455 <nandacumaar367@gmail.com>
2025-05-09 23:10:18 +0530

    Your message here

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './ArtCard.css';
 
 const ArtCard = ({ art }) => {
   return (
-    <div className="card">
+    <motion.div
+      className="card"
+      whileHover={{ scale: 1.05 }}
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+    >
       <img src={art.image} alt={art.title} />
       <h3>{art.title}</h3>
       <p>by {art.artist}</p>
-    </div>
+    </motion.div>
   );
 };
 
--- src/data/artworks.js
+++ src/data/artworks.js
@@ -16,6 +16,108 @@ const artworks = [
       title: "Girl with a Pearl Earring",
       artist: "Johannes Vermeer",
       image: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Meisje_met_de_parel.jpg"
+    },
+    {
+      id: 4,
+      title: "The Scream",
+      artist: "Edvard Munch",
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/f4/The_Scream.jpg"
+    },
+    {
+      id: 5,
+      title: "Mona Lisa",
+      artist: "Leonardo da Vinci",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
+    },
+    {
+      id: 6,
+      title: "Guernica",
+      artist: "Pablo Picasso",
+      image: "https://upload.wikimedia.org/wikipedia/en/7/74/PicassoGuernica.jpg"
+    },
+    {
+      id: 7,
+      title: "The Kiss",
+      artist: "Gustav Klimt",
+      image: "https://upload.wikimedia.org/wikipedia/commons/7/70/Gustav_Klimt_016.jpg"
+    },
+    {
+      id: 8,
+      title: "American Gothic",
+      artist: "Grant Wood",
+      image: "https://upload.wikimedia.org/wikipedia/commons/7/75/Grant_DeVolson_Wood_-_American_Gothic.jpg"
+    },
+    {
+      id: 9,
+      title: "The Night Watch",
+      artist: "Rembrandt",
+      image: "https://upload.wikimedia.org/wikipedia/commons/2/23/The_Nightwatch_by_Rembrandt.jpg"
+    },
+    {
+      id: 10,
+      title: "Water Lilies",
+      artist: "Claude Monet",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Claude_Monet_-_Water_Lilies_-_1906.jpg"
+    },
+    {
+      id: 11,
+      title: "The Birth of Venus",
+      artist: "Sandro Botticelli",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/1c/Birth_of_Venus_Botticelli.jpg"
+    },
+    {
+      id: 12,
+      title: "The Creation of Adam",
+      artist: "Michelangelo",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Michelangelo_-_Creation_of_Adam.jpg"
+    },
+    {
+      id: 13,
+      title: "The Great Wave off Kanagawa",
+      artist: "Hokusai",
+      image: "https://upload.wikimedia.org/wikipedia/commons/0/0a/Great_Wave_off_Kanagawa2.jpg"
+    },
+    {
+      id: 14,
+      title: "The Hay Wain",
+      artist: "John Constable",
+      image: "https://upload.wikimedia.org/wikipedia/commons/2/24/The_Hay_Wain.jpg"
+    },
+    {
+      id: 15,
+      title: "The Garden of Earthly Delights",
+      artist: "Hieronymus Bosch",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/1e/El_jard%C3%ADn_de_las_Delicias.jpg"
+    },
+    {
+      id: 16,
+      title: "Las Meninas",
+      artist: "Diego Velázquez",
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Las_Meninas_01.jpg"
+    },
+    {
+      id: 17,
+      title: "Whistler’s Mother",
+      artist: "James McNeill Whistler",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/19/Whistlers_Mother_high_res.jpg"
+    },
+    {
+      id: 18,
+      title: "The Dance",
+      artist: "Henri Matisse",
+      image: "https://upload.wikimedia.org/wikipedia/en/7/76/Matisse_-_The_Dance_I.jpg"
+    },
+    {
+      id: 19,
+      title: "Composition VIII",
+      artist: "Wassily Kandinsky",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Kandinsky_1923_-_Composition_8.jpg"
+    },
+    {
+      id: 20,
+      title: "Bal du moulin de la Galette",
+      artist: "Pierre-Auguste Renoir",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Auguste_Renoir_-_Dance_at_Le_Moulin_de_la_Galette.jpg"
     }
   ];
   export default artworks;
