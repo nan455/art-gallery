88c786f6eec8d0cc9678ecb5c7219f1b79fa0698
nan455 <nandacumaar367@gmail.com>
2025-05-10 23:44:25 +0530

    submitted

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,7 @@ app.use('/api/artworks', require('./routes/artworks'));
 
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
